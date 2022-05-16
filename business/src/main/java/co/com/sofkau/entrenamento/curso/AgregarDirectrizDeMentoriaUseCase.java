b6ce6bb9faf1b1ddd65158ff02d2a94fc957e57a
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-16 16:15:15 -0500

    Caso de uso
    Se agrega el caso de uso de agregar directriz de mentoria

--- business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
@@ -1,2 +1,22 @@
-package co.com.sofkau.entrenamento.curso;public class AgregarDirectrizDeMentoriaUseCase {
+package co.com.sofkau.entrenamento.curso;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.Curso;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizDeMentoria;
+
+public class AgregarDirectrizDeMentoriaUseCase extends UseCase<RequestCommand<AgregarDirectrizDeMentoria>, ResponseEvents> {
+
+    @Override
+    public void executeUseCase(RequestCommand<AgregarDirectrizDeMentoria> agregarDirectrizDeMentoriaRequestCommand) {
+        var command = agregarDirectrizDeMentoriaRequestCommand.getCommand();
+        var curso = Curso.from(
+                command.getCursoId(), repository().getEventsBy(command.getCursoId().value())
+        );
+        curso.agregarDirectrizDeMentoria(command.getMentoriaId(), command.getDirectriz());
+
+        emit().onResponse(new ResponseEvents(curso.getUncommittedChanges()));
+    }
+
 }
