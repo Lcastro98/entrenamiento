c000b232075433de395ee96c4d99c60d8ce11820
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-05-16 15:41:59 -0500

    Comando
    Se agrega el comando AgregarDirectrizDeMentoria

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/commands/AgregarDirectrizDeMentoria.java
@@ -1,2 +1,30 @@
-package co.com.sofkau.entrenamiento.curso.commands;public class AgregarDirectrizDeMentoria {
+package co.com.sofkau.entrenamiento.curso.commands;
+
+import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.entrenamiento.curso.values.CursoId;
+import co.com.sofkau.entrenamiento.curso.values.Directriz;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
+
+public class AgregarDirectrizDeMentoria extends Command {
+    private final CursoId cursoId;
+    private final MentoriaId mentoriaId;
+    private final Directriz directriz;
+
+    public AgregarDirectrizDeMentoria(CursoId cursoId, MentoriaId mentoriaId, Directriz directriz) {
+        this.cursoId = cursoId;
+        this.mentoriaId = mentoriaId;
+        this.directriz = directriz;
+    }
+
+    public CursoId getCursoId() {
+        return cursoId;
+    }
+
+    public MentoriaId getMentoriaId() {
+        return mentoriaId;
+    }
+
+    public Directriz getDirectriz() {
+        return directriz;
+    }
 }
